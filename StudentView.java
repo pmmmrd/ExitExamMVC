1a78a6e6ba70be3afc3711d5e8d7144a1fe5d12f
pmmmrd <66050920@kmitl.ac.th>
2025-09-20 16:16:05 +0700

    StudentView.java

--- StudentView.java
+++ StudentView.java
@@ -12,5 +12,17 @@ public class StudentView {
     public void showMessage(String msg) {
         System.out.println(msg);
     }
+
+    public void showEnrollments(Student student, List<Enrollment> enrollments) {
+        System.out.println("---------------- ประวัติการลงทะเบียนของ " + student.getFullName() + " ----------------");
+        for (Enrollment e : enrollments) {
+            if (e.getStudent().equals(student)) {
+                System.out.print(e.getSubject().getName());
+                if (e.getGrade() != null) System.out.print(" | เกรด: " + e.getGrade());
+                System.out.println();
+            }
+        }
+    }
+
 }
 
